a208f0972f56f751254e27a792c2f3f93927d3d2
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2023-08-27 01:38:58 +0200

    Added Debug Utils

---
+++ debug/debug.go
@@ -0,0 +1,21 @@
+package debug
+
+var DebugMode bool = false
+
+func SetDebugMode(mode bool) {
+    DebugMode = mode
+}
+
+func GetDebugMode() bool {
+    return DebugMode
+}
+
+func ExecuteInDebugMode(f func ()) {
+    if DebugMode {
+        f()
+    }
+}
+
+func ExecuteAndPassthrough(f func([]any) []any, values ...any) []any {
+    return f(values)
+}
