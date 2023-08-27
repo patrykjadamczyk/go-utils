8ee7da2b66917d56c92b4e2e8030977befcbe52c
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2023-08-27 06:06:32 +0200

    Remove * from not needed places in Result type

--- types/errors/result.go
+++ types/errors/result.go
@@ -32,31 +32,31 @@ func (r *Result[T]) Ok(value T) Result[T] {
 	return *r
 }
 
-func (r *Result[T]) IsError() bool {
+func (r Result[T]) IsError() bool {
 	return utils.IsError(r.ErrorValue)
 }
 
-func (r *Result[T]) Expect(err any) {
+func (r Result[T]) Expect(err any) {
 	if r.IsError() {
 		panic(err)
 	}
 }
 
-func (r *Result[T]) Unwrap() T {
+func (r Result[T]) Unwrap() T {
 	if r.IsError() {
 		panic(errors.UnwrapError{})
 	}
 	return r.DataValue
 }
 
-func (r *Result[T]) UnwrapOr(defaultVal T) T {
+func (r Result[T]) UnwrapOr(defaultVal T) T {
 	if r.IsError() {
 		return defaultVal
 	}
 	return r.DataValue
 }
 
-func (r *Result[T]) AndThen(fn func(T) any) any {
+func (r Result[T]) AndThen(fn func(T) any) any {
 	if r.IsError() {
 		return nil
 	}
