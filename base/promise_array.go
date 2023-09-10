ee4f2e0092943db6c454001b5470dabce8656e8f
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2023-09-10 22:35:22 +0200

    Fixed a bug in PromiseAll returning invalid values and bug with wait

--- base/promise_array.go
+++ base/promise_array.go
@@ -2,17 +2,17 @@ package base
 
 // Run all promises and wait for their results
 func PromiseAll(functions ...any) ([]any, []error) {
-    promises := make([]*Promise, len(functions))
-    results := make([]any, len(functions))
-    errors := make([]error, len(functions))
-    for i, f := range functions {
-        promises[i] = MakePromisePointer(f)
-        promises[i].Call()
-        results[i] = promises[i].Result
-        errors[i] = promises[i].Error
-    }
-    for _, p := range promises {
-        p.Wait()
-    }
-    return results, errors
+	promises := make([]*Promise, len(functions))
+	results := make([]any, len(functions))
+	errors := make([]error, len(functions))
+	for i, f := range functions {
+		promises[i] = MakePromisePointer(f)
+		promises[i].Call()
+	}
+	for i := 0; i < len(functions); i++ {
+		promises[i].Wait()
+		results[i] = promises[i].Result
+		errors[i] = promises[i].Error
+	}
+	return results, errors
 }
