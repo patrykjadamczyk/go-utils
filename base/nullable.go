55560ce88599d97236acaccb648e6deea5fe3349
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2023-09-10 22:36:20 +0200

    Removed pointer reference in UnwrapOr

--- base/nullable.go
+++ base/nullable.go
@@ -117,7 +117,7 @@ func (n Nullable[T]) Unwrap() T {
 }
 
 // Get underlying value or return default value if error is found
-func (n *Nullable[T]) UnwrapOr(defaultVal T) T {
+func (n Nullable[T]) UnwrapOr(defaultVal T) T {
 	if !n.Valid {
 		return defaultVal
 	}
