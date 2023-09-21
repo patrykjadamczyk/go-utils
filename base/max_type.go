35ad9015afddf97edfbdb0a6927ccf473876ecfd
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2023-09-21 11:23:55 +0200

    Added Max and Min for Type utilities

---
+++ base/max_type.go
@@ -0,0 +1,100 @@
+package base
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+)
+
+type NumericType interface {
+	~int8 | ~int16 | ~int32 | ~int64 | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64 | ~int | ~uint
+}
+
+type AnyFloat interface {
+	~float32 | ~float64
+}
+
+type AnyInt interface {
+	~int8 | ~int16 | ~int32 | ~int64 | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~int | ~uint
+}
+
+// GetMaxForType
+// Get Max Value for specified numeric type
+func GetMaxForType[T NumericType]() T {
+	// Note: This is wrapper arround implementation to trick generics in using just type parameter for getting this value
+	var test T
+	return EnsureType[T](getMaxForType(test))
+}
+
+// GetMaxForType Implementation
+func getMaxForType(value any) any {
+	switch value.(type) {
+	case float64:
+		return float64(math.MaxFloat64)
+	case float32:
+		return float32(math.MaxFloat32)
+	case int:
+		return int(math.MaxInt)
+	case int8:
+		return int8(math.MaxInt8)
+	case int16:
+		return int16(math.MaxInt16)
+	case int32:
+		return int32(math.MaxInt32)
+	case int64:
+		return int64(math.MaxInt64)
+	case uint:
+		return uint(math.MaxUint)
+	case uint8:
+		return uint8(math.MaxUint8)
+	case uint16:
+		return uint16(math.MaxUint16)
+	case uint32:
+		return uint32(math.MaxUint32)
+	case uint64:
+		return uint64(math.MaxUint64)
+	default:
+		panic(fmt.Sprintf("Invalid Type %s", reflect.TypeOf(value).Name()))
+	}
+}
+
+// GetMinForType
+// Get Min Value for specified numeric type
+func GetMinForType[T NumericType]() T {
+	// Note: This is wrapper arround implementation to trick generics in using just type parameter for getting this value
+	var test T
+	return EnsureType[T](getMinForType(test))
+}
+
+// GetMinForType Implementation
+func getMinForType(value any) any {
+	switch value.(type) {
+	case float64:
+		return -1 * float64(math.MaxFloat64)
+	case float32:
+		return -1 * float32(math.MaxFloat32)
+	case int:
+		return int(math.MinInt)
+	case int8:
+		return int8(math.MinInt8)
+	case int16:
+		return int16(math.MinInt16)
+	case int32:
+		return int32(math.MinInt32)
+	case int64:
+		return int64(math.MinInt64)
+	case uint:
+		return uint(0)
+	case uint8:
+		return uint8(0)
+	case uint16:
+		return uint16(0)
+	case uint32:
+		return uint32(0)
+	case uint64:
+		return uint64(0)
+	default:
+		panic(fmt.Sprintf("Invalid Type %s", reflect.TypeOf(value).Name()))
+	}
+}
+
---
+++ base_test/max_type_test.go
@@ -0,0 +1,125 @@
+package base_test
+
+import (
+	"testing"
+	"fmt"
+	"math"
+	. "github.com/patrykjadamczyk/go-utils/base"
+)
+
+func TestMaxType(t *testing.T) {
+	testCases := []struct {
+		input    int64
+		expected int64
+	}{
+		{input: int64(GetMaxForType[int]()),    expected: math.MaxInt},
+		{input: int64(GetMaxForType[int8]()),   expected: math.MaxInt8},
+		{input: int64(GetMaxForType[int16]()),  expected: math.MaxInt16},
+		{input: int64(GetMaxForType[int32]()),  expected: math.MaxInt32},
+		{input: int64(GetMaxForType[int64]()),  expected: math.MaxInt64},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("Test MaxType i%d", i), func(t *testing.T) {
+			if tc.input != tc.expected {
+				t.Errorf("i%d: Expected %d, but got %d", i, tc.expected, tc.input)
+			}
+		})
+	}
+	testCases2 := []struct {
+		input    uint64
+		expected uint64
+	}{
+		{input: uint64(GetMaxForType[uint8]()),  expected: uint64(math.MaxUint8)},
+		{input: uint64(GetMaxForType[uint16]()), expected: uint64(math.MaxUint16)},
+		{input: uint64(GetMaxForType[uint32]()), expected: uint64(math.MaxUint32)},
+	}
+
+	for i, tc := range testCases2 {
+		t.Run(fmt.Sprintf("Test MaxType ui%d", i), func(t *testing.T) {
+			if tc.input != tc.expected {
+				t.Errorf("ui%d: Expected %d, but got %d", i, tc.expected, tc.input)
+			}
+		})
+	}
+	testCases3 := []struct {
+		input    float64
+		expected float64
+	}{
+		{input: float64(GetMaxForType[float32]()), expected: float64(math.MaxFloat32)},
+		{input: GetMaxForType[float64](), expected: float64(math.MaxFloat64)},
+	}
+
+	for i, tc := range testCases3 {
+		t.Run(fmt.Sprintf("Test MaxType f%d", i), func(t *testing.T) {
+			if tc.input != tc.expected {
+				t.Errorf("f%d: Expected %e, but got %e", i, tc.expected, tc.input)
+			}
+		})
+	}
+	test1i := GetMaxForType[uint]()
+	t.Run("Test MaxType a1", func(t *testing.T) {
+		if test1i != math.MaxUint {
+			t.Errorf("a1: Expected math.MaxUint, but got %d", test1i)
+		}
+	})
+	test2i := GetMaxForType[uint64]()
+	t.Run("Test MaxType a2", func(t *testing.T) {
+		if test2i != math.MaxUint64 {
+			t.Errorf("a2: Expected math.MaxUint64, but got %d", test1i)
+		}
+	})
+}
+
+func TestMinType(t *testing.T) {
+	testCases := []struct {
+		input    int64
+		expected int64
+	}{
+		{input: int64(GetMinForType[int]()),    expected: math.MinInt},
+		{input: int64(GetMinForType[int8]()),   expected: math.MinInt8},
+		{input: int64(GetMinForType[int16]()),  expected: math.MinInt16},
+		{input: int64(GetMinForType[int32]()),  expected: math.MinInt32},
+		{input: int64(GetMinForType[int64]()),  expected: math.MinInt64},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("Test MinType i%d", i), func(t *testing.T) {
+			if tc.input != tc.expected {
+				t.Errorf("i%d: Expected %d, but got %d", i, tc.expected, tc.input)
+			}
+		})
+	}
+	testCases2 := []struct {
+		input    uint64
+	}{
+		{input: uint64(GetMinForType[uint]())},
+		{input: uint64(GetMinForType[uint8]())},
+		{input: uint64(GetMinForType[uint16]())},
+		{input: uint64(GetMinForType[uint32]())},
+		{input: uint64(GetMinForType[uint64]())},
+	}
+
+	for i, tc := range testCases2 {
+		t.Run(fmt.Sprintf("Test MinType ui%d", i), func(t *testing.T) {
+			if tc.input != 0 {
+				t.Errorf("ui%d: Expected 0, but got %d", i, tc.input)
+			}
+		})
+	}
+	testCases3 := []struct {
+		input    float64
+		expected float64
+	}{
+		{input: float64(GetMinForType[float32]()), expected: -1 * float64(math.MaxFloat32)},
+		{input: GetMinForType[float64](), expected: -1 * float64(math.MaxFloat64)},
+	}
+
+	for i, tc := range testCases3 {
+		t.Run(fmt.Sprintf("Test MinType f%d", i), func(t *testing.T) {
+			if tc.input != tc.expected {
+				t.Errorf("f%d: Expected %e, but got %e", i, tc.expected, tc.input)
+			}
+		})
+	}
+}
