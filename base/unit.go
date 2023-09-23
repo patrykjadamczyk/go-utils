51659b9f4bad85dd0ce105fcee64d4f1bc3f7cbd
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2023-09-23 02:17:01 +0200

    Added MakeUnit and NormalizeUnit functions

--- base/unit.go
+++ base/unit.go
@@ -34,6 +34,31 @@ func (u UnitValue[T]) GoString() string {
 	)
 }
 
+// Note that some value is in specific unit like seconds
 func Unit[T any](value T, unitName string) UnitValue[T] {
 	return UnitValue[T]{Value: value, UnitName: unitName}
+}
+
+// Make function that will return unit
+// This is factory of method to make function for specific unit
+func MakeUnit[T any](unitName string) func(T) UnitValue[T] {
+	return func(value T) UnitValue[T] {
+		return Unit[T](value, unitName)
+	}
+}
+
+// Make function that will normalize to unit type
+func NormalizeUnit[T any](unitName string) func(any) Result[UnitValue[T]] {
+	return func(value any) Result[UnitValue[T]] {
+		if uv, ok := value.(UnitValue[T]); ok {
+			if uv.GetUnitName() == unitName {
+				return Ok[UnitValue[T]](uv)
+			}
+			return Err[UnitValue[T]](NewError("Invalid Unit"))
+		}
+		if v, ok := value.(T); ok {
+			return Ok[UnitValue[T]](Unit[T](v, unitName))
+		}
+		return Err[UnitValue[T]](NewError("Invalid Type"))
+	}
 }
--- base_test/unit_test.go
+++ base_test/unit_test.go
@@ -13,4 +13,29 @@ func TestUnitType(t *testing.T) {
 	if test1.GetUnitName() != "seconds" {
 		t.Errorf("Expected seconds, but got %s", test1.GetUnitName())
 	}
+	tf2 := MakeUnit[int]("minutes")
+	test2 := tf2(2)
+	if test2.GetValue() != 2 {
+		t.Errorf("Expected 2, but got %d", test2.GetValue())
+	}
+	if test2.GetUnitName() != "minutes" {
+		t.Errorf("Expected minutes, but got %s", test2.GetUnitName())
+	}
+	tf3 := func (v any) Result[UnitValue[int]] {
+		value := NormalizeUnit[int]("hours")(v)
+		if value.IsError() {
+			return Err[UnitValue[int]](value.GetError())
+		}
+		return Ok[UnitValue[int]](value.Unwrap())
+	}
+	test3 := tf3(3)
+	if test3.IsError() {
+		t.Errorf("Expected Ok, but got %s", test3.GetError())
+	}
+	if test3.Unwrap().GetValue() != 3 {
+		t.Errorf("Expected 3, but got %d", test3.Unwrap().GetValue())
+	}
+	if test3.Unwrap().GetUnitName() != "hours" {
+		t.Errorf("Expected hours, but got %s", test3.Unwrap().GetUnitName())
+	}
 }
