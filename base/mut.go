ecd40586fc3200ff7948a9d1286cd4d1139b93da
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2024-05-25 11:23:34 +0000

    Changed how MutableRefValue works to use pointer instead of slice

--- base/mut.go
+++ base/mut.go
@@ -2,38 +2,34 @@ package base
 
 // MutableRefValue is structure for Mutable Value that acts like reference
 type MutableRefValue[T any] struct {
-	Data []T
+	Data *T
 }
 
 // Set value
 func (mrv *MutableRefValue[T]) Set(value T) {
-	if mrv.Data == nil || len(mrv.Data) == 0 {
-		mrv.Data = make([]T, 0)
-		mrv.Data = append(mrv.Data, value)
+	if mrv.Data == nil {
+		mrv.Data = &value
 		return
 	}
-	mrv.Data[0] = value
+	*mrv.Data = value
 }
 
 // Get value
 func (mrv MutableRefValue[T]) Get() T {
-	if mrv.Data == nil || len(mrv.Data) == 0 {
+	if mrv.Data == nil {
 		return Null[T]().Data
 	}
-	return mrv.Data[0]
+	return *mrv.Data
 }
 
 // Get pointer to value
 func (mrv *MutableRefValue[T]) GetPtr() *T {
-	if mrv.Data == nil || len(mrv.Data) == 0 {
-		return nil
-	}
-	return &mrv.Data[0]
+	return mrv.Data
 }
 
 // MakeRefValue makes mutable reference to specified value still acting completely like value
 func MutRefValue[T any](value T) MutableRefValue[T] {
-	data := make([]T, 0)
-	data = append(data, value)
-	return MutableRefValue[T]{Data: data}
+	// data := make([]T, 0)
+	// data = append(data, value)
+	return MutableRefValue[T]{Data: &value}
 }
