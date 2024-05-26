3b0480995a3ff79dc7ec9065396866f1069ca37d
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2024-05-26 05:33:16 +0000

    WIP: UniversalEquals part 2

--- base/equality.go
+++ base/equality.go
@@ -1,6 +1,10 @@
 package base
 
-import "github.com/patrykjadamczyk/go-utils/datastructures/interfaces"
+import (
+	"reflect"
+
+	"github.com/patrykjadamczyk/go-utils/datastructures/interfaces"
+)
 
 // UniversalEquals is a function that determins if two values are equal
 // It checks through == operator, different interfaces and other things
@@ -11,60 +15,58 @@ import "github.com/patrykjadamczyk/go-utils/datastructures/interfaces"
 // This means that if you want you can use data value of result directly as condition result
 // And if you want to have special case for not determined operation you can check for res.IsError()
 func UniversalEquals(v1 any, v2 any) Result[bool] {
-	// Special shortcurcuit cases for v2
-	switch v2t := v2.(type) {
-		// Value.Get() any -> wrapped type
-	case interfaces.IGetAnyFunc:
-		return UniversalEquals(v1, v2t.GetAny())
-		// Value.GetAny() any -> wrapped type
-	case interfaces.IGetAny:
-		return UniversalEquals(v1, v2t.Get())
+	// Shortcircuit for nil
+	if v1 == nil || v2 == nil {
+		return Ok(v1 == nil && v2 == nil)
 	}
+	// Shortcircuit for Wrappers
+	if v1i, ok := v1.(interfaces.IGetAny); ok {
+		if v2i, ok := v2.(interfaces.IGetAny); ok {
+			return UniversalEquals(v1i.Get(), v2i.Get())
+		}
+		if v2i, ok := v2.(interfaces.IGetAnyFunc); ok {
+			return UniversalEquals(v1i.Get(), v2i.GetAny())
+		}
+	}
+	if v1i, ok := v1.(interfaces.IGetAnyFunc); ok {
+		if v2i, ok := v2.(interfaces.IGetAny); ok {
+			return UniversalEquals(v1i.GetAny(), v2i.Get())
+		}
+		if v2i, ok := v2.(interfaces.IGetAnyFunc); ok {
+			return UniversalEquals(v1i.GetAny(), v2i.GetAny())
+		}
+	}
+	// Shortcircuit for objects that implement Equals(any) or EqualsAny(any)
+	if v1i, ok := v1.(interfaces.IEqualsAny); ok {
+		return Ok(v1i.Equals(v2))
+	}
+	if v2i, ok := v2.(interfaces.IEqualsAny); ok {
+		return Ok(v2i.Equals(v1))
+	}
+	// Check for all basic go types
+	// comparable types
 	switch v1t := v1.(type) {
-		// comparable types - Go Types
-	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64, string, bool, uintptr, complex64, complex128, nil:
+	case int, int8, int16, int32, int64:
 		switch v2t := v2.(type) {
-		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64, string, bool, uintptr, complex64, complex128, nil:
+		case int, int8, int16, int32, int64:
 			return Ok(v1t == v2t)
-		case interfaces.IEqualsAny:
-			return Ok(v2t.Equals(v1t))
-		case interfaces.IEqualsAnyFunc:
-			return Ok(v2t.EqualsAny(v1t))
-		default:
-			return ErrWithValue(false, NewError("Unsupported operation v1 == v2"))
 		}
-		// Value.Equals(other any)
-	case interfaces.IEqualsAny:
+	case uint, uint8, uint16, uint32, uint64, uintptr:
 		switch v2t := v2.(type) {
-		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64, string, bool, uintptr, complex64, complex128, nil:
-			return Ok(v1t.Equals(v2t))
-		case interfaces.IEqualsAny:
-			return Ok(v1t.Equals(v2t))
-		case interfaces.IEqualsAnyFunc:
-			return Ok(v1t.Equals(v2t))
-		default:
-			return ErrWithValue(false, NewError("Unsupported operation v1 == v2"))
+		case uint, uint8, uint16, uint32, uint64, uintptr:
+			return Ok(v1t == v2t)
 		}
-		// Value.EqualsAny(other any)
-	case interfaces.IEqualsAnyFunc:
+
+	case float32, float64, string, bool, complex64, complex128, error:
 		switch v2t := v2.(type) {
-		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64, string, bool, uintptr, complex64, complex128, nil:
-			return Ok(v1t.EqualsAny(v2t))
-		case interfaces.IEqualsAny:
-			return Ok(v1t.EqualsAny(v2t))
-		case interfaces.IEqualsAnyFunc:
-			return Ok(v1t.EqualsAny(v2t))
-		default:
-			return ErrWithValue(false, NewError("Unsupported operation v1 == v2"))
+		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64, string, bool, uintptr, complex64, complex128, error:
+			return Ok(v1t == v2t)
 		}
-		// Value.Get() any -> wrapped type
-	case interfaces.IGetAny:
-		return UniversalEquals(v1t.Get(), v2)
-		// Value.GetAny() any -> wrapped type
-	case interfaces.IGetAnyFunc:
-		return UniversalEquals(v1t.GetAny(), v2)
-		// Couldn't be determined
-	default:
-		return ErrWithValue(false, NewError("Unsupported operation v1 == v2"))
 	}
+	// Reflection based equality
+	if reflect.DeepEqual(v1, v2) {
+		return Ok(true)
+	}
+	// Couldn't be determined
+	return ErrWithValue(false, NewError("Unsupported operation v1 == v2"))
 }
