5e9a1510905150e53fa35f5bb57ef4b5d33b5b2d
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2024-05-30 08:39:02 +0000

    Added ToTuple5-16

--- datastructures/interfaces/tuple.go
+++ datastructures/interfaces/tuple.go
@@ -11,3 +11,51 @@ type IToTuple3[T1, T2, T3 any] interface {
 type IToTuple4[T1, T2, T3, T4 any] interface {
 	ToTuple() (T1, T2, T3, T4)
 }
+
+type IToTuple5[T1, T2, T3, T4, T5 any] interface {
+	ToTuple() (T1, T2, T3, T4, T5)
+}
+
+type IToTuple6[T1, T2, T3, T4, T5, T6 any] interface {
+	ToTuple() (T1, T2, T3, T4, T5, T6)
+}
+
+type IToTuple7[T1, T2, T3, T4, T5, T6, T7 any] interface {
+	ToTuple() (T1, T2, T3, T4, T5, T6, T7)
+}
+
+type IToTuple8[T1, T2, T3, T4, T5, T6, T7, T8 any] interface {
+	ToTuple() (T1, T2, T3, T4, T5, T6, T7, T8)
+}
+
+type IToTuple9[T1, T2, T3, T4, T5, T6, T7, T8, T9 any] interface {
+	ToTuple() (T1, T2, T3, T4, T5, T6, T7, T8, T9)
+}
+
+type IToTuple10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10 any] interface {
+	ToTuple() (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)
+}
+
+type IToTuple11[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11 any] interface {
+	ToTuple() (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11)
+}
+
+type IToTuple12[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12 any] interface {
+	ToTuple() (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12)
+}
+
+type IToTuple13[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13 any] interface {
+	ToTuple() (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13)
+}
+
+type IToTuple14[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14 any] interface {
+	ToTuple() (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14)
+}
+
+type IToTuple15[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15 any] interface {
+	ToTuple() (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15)
+}
+
+type IToTuple16[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16 any] interface {
+	ToTuple() (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16)
+}
