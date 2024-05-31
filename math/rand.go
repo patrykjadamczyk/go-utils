d83a6f1aa7c601ec59ea4d622ed66b12ec8d8d13
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2024-05-31 18:37:06 +0000

    Fixed type of GetRandomNumber

--- math/rand.go
+++ math/rand.go
@@ -59,7 +59,7 @@ func getRandomNumber[
 }
 
 func GetRandomNumber[
-	T ~int8 | ~int16 | ~int32 | ~int64 | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64 | ~int | ~uint,
+	T ~int8 | ~int16 | ~int32 | ~int64 | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64 | ~int | ~uint | ~complex64 | ~complex128,
 ]() T {
 	if v, ok := getRandomNumber[T]().(T); ok {
 		return v
