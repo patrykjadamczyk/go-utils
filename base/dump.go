ce52f51edfccfb8438d491c13573fb694c2aa1e5
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2024-10-19 01:40:08 +0200

    Correct Dump so it skippes lib code

--- base/dump.go
+++ base/dump.go
@@ -4,15 +4,26 @@ import (
 	"github.com/gookit/goutil/dump"
 )
 
+// defaultSkip is the default number of stack frames to skip
+// This constant is meant to be directly copied from the source code line below
+// ~/go/pkg/mod/github.com/gookit/goutil@<ver>/dump/dump.go:24
+const defaultSkip = 3
+
+// This constant is meant to be tested manually and changed so it shows correct information
+const DevDumpSkip = defaultSkip + 0
+
 // Dump variable to console output
 func Dump(vs ...any) {
-	dump.P(vs...)
+	dump.NewWithOptions(
+		dump.WithCallerSkip(DevDumpSkip),
+	).Print(vs...)
 }
 
 // Dump variable to console output with specified depth
 func DumpDepth(depth int, vs ...any) {
 	dump.NewWithOptions(
 		func(opts *dump.Options) {
+			opts.CallerSkip = DevDumpSkip
 			opts.MaxDepth = depth
 		},
 	).Print(vs...)
@@ -20,5 +31,10 @@ func DumpDepth(depth int, vs ...any) {
 
 // Dump variable to console output with unlimited depth
 func DumpAll(vs ...any) {
-	DumpDepth(GetMaxForType[int](), vs...)
+	dump.NewWithOptions(
+		func(opts *dump.Options) {
+			opts.CallerSkip = DevDumpSkip
+			opts.MaxDepth = GetMaxForType[int]()
+		},
+	).Print(vs...)
 }
